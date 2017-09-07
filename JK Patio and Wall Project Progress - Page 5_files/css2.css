be38cd2d374b644c1bc59861ae5b2530ffc95c71
jimkilleen <james.j.killeen@gmail.com>
2017-09-07 15:36:09 -0400

    Modified links  in remaining files and moved over renamed css files to each folder.

---
+++ JK Patio and Wall Project Progress - Page 5_files/css2.css
@@ -0,0 +1,45 @@
+.vbcom_footerlinks
+{
+background: #f5f5f5 none   ;
+margin-top:0px;
+color: #5d5d5d;
+}
+
+.vbcom_footerlinks a
+{
+	color:#000000;
+	text-decoration:none;
+}
+.vbcom_footerlinks a:hover
+{
+	color:#473D2F;
+	text-decoration:underline;
+}
+
+.navtabs li a.navtab_purchase
+{
+color: #fbcb3d;
+}
+.navtabs li a.navtab_purchase:hover
+{
+color: black;
+}
+
+.below_body a {color:#dddddd;}
+
+/* Background Color for CKEditor */
+.cke_skin_kama .cke_wrapper {
+background:#24242E url(images/buttons/newbtn_middle.png) repeat-x   !important;
+}
+
+/* Background Color for CKEditor Buttons (Note, this may be too dark for some styles. If so you can delete this one.) */
+.cke_skin_kama .cke_toolgroup {
+//background-color: #xxxxxx !important;
+background: #998F82 none    !important;
+}
+
+/* Background Color for CKEditor Hover Link*/
+.cke_skin_kama a:hover {
+background: #473D2F url(images/buttons/newbtn_middle.png) repeat-x left top !important;
+}
+ 
